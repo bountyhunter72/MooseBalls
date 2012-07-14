5aa0b9ffab97c11fa3505ff745bbe4c302848dc9
John <jpatterson72@charter.net>
2012-07-14 13:10:12 -0400

    Updated MooseBall AFK Timer, fixed glitches

---
+++ MooseBalls/src/main/java/net/charter/jpatterson72/MooseBall/MooseBallAFKInitTimer.java
@@ -0,0 +1,32 @@
+package net.charter.jpatterson72.MooseBall;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.plugin.Plugin;
+
+public class MooseBallAFKInitTimer {
+	
+	public static void MooseBallAFKInitTimer(final Plugin myPlugin, final Player player) {
+		final int timerID = myPlugin.getServer().getScheduler()
+				.scheduleSyncDelayedTask(myPlugin, new Runnable() {
+
+					public void run() {
+						player.setMetadata("MooseBallAFK", new FixedMetadataValue(
+								myPlugin, "true"));
+
+					}
+					
+					
+					
+				}, 20);
+		
+
+		return;
+	}
+}
+
+
+
+
+
